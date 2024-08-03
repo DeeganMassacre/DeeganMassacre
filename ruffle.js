fa1c397bcc6a52efd1aa363f4d9a11fc8bcc25b0
DeeganMassacre <deegan.gammill12@gmail.com>
2024-08-03 05:27:07 -0500

    Update ruffle.js

--- ruffle.js
+++ ruffle.js
@@ -26,3 +26,36 @@
     </script>
         <player.load (I spin head.swf);></player.load>
     <embed src="I spin head.swf"></body></embed>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset='utf8'>
+    </head>
+    <body>
+    
+    <div id="container"> </div>
+
+       <script>
+          window.RufflePlayer = {
+            config: {
+              autoplay: "on",
+            wmode: "transparent",
+            scale: "showAll",
+              unmuteOverlay: "hidden",
+            }
+          };
+       </script>
+    
+       <script>
+          window.RufflePlayer = window.RufflePlayer || {};
+          window.addEventListener("load", (event) => {
+              const ruffle = window.RufflePlayer.newest();
+              const player = ruffle.createPlayer();
+              const container = document.getElementById("container");
+              container.appendChild(player);
+              player.load("index.swf");
+          });
+      </script>
+      <script src="ruffle/ruffle.js"></script>
+    </body>
+</html>
