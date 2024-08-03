260ed6ba2b25087bcab319cd1c4c2f567924471a
DeeganMassacre <deegan.gammill12@gmail.com>
2024-08-03 04:02:48 -0500

    Add files via upload

---
+++ ruffle.js
@@ -0,0 +1,28 @@
+<script src=".ruffle.js"></script>
+<script>
+    window.RufflePlayer = window.RufflePlayer || {};
+
+    window.addEventListener("DOMContentLoaded", () => {
+        let ruffle = window.RufflePlayer.newest();
+        let player = ruffle.createPlayer();
+        let container = document.getElementById("container");
+        container.appendChild(player);
+        player.load("movie.swf");
+    });
+</script>
+<script src="path/to/ruffle/ruffle.js"></script>
+<script src="./ruffle.js"></script>
+    <script src="flash player 32.exe"></script>
+    <div id="containter"></div>
+    <script>
+        window.RufflePlayer = window.RufflePlayer || {};
+        window.addEventListener("load", (event) => {
+            const ruffle = window.RufflePlayer.newest();
+            const player = ruffle.createPlayer();
+            const container = document.getElementById("container");
+            container.appendChild(player);
+            player.load("movie.swf");
+        });
+    </script>
+        <player.load (I spin head.swf);></player.load>
+    <embed src="I spin head.swf"></body></embed>
